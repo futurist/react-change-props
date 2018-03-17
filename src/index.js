0893384de9fd840a33689035a049c8956bb4799d
futurist <jamesyang999@gmail.com>
2018-03-17 20:37:26 +0800

    clean comments

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 import React from 'react'
 
-
 const {isArray} = Array
 const {isFrozen, assign} = Object
-
-/** Try to Wrap Component Class, failed? */
-// const isFunction = fn=>typeof fn==='function'
-// const isClassType = (Component) => {
-//   return 'prototype' in Component && isFunction(Component.prototype.render)
-// }
-
-// const wrapComponent = (Component) => {
-//   const CSSObjComponent = isClassType(Component)
-//   ? class extends Component {
-//     render () {
-//       const result = super.render()
-//       return injectCSS(result)
-//     }
-//   }
-//   : function (props) {
-//     const result = Component.apply(this, arguments)
-//     return injectCSS(result)
-//   }
-//   return assign(CSSObjComponent, Component)
-// }
 
 const assignProps = (element, cb)=>{
   var children
